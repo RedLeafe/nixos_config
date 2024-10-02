ef9a3e193bc7254ac2b12c52eb6dd6cdd3e61cf1
BirdeeHub <robindbor@gmail.com>
2024-10-02 16:00:26 -0700

    Committing in accordance with the prophecy.
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   systems/vm.nix

--- systems/vm.nix
+++ systems/vm.nix
@@ -72,18 +72,20 @@ in {
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  networking.networkmanager.enable = true;
-  # networking = {
-  #   dhcpcd.enable = false;
-  #   interfaces.eth0.ipv4.addresses = [{
-  #     address = "192.168.220.223";
-  #     prefixLength = 24;
-  #   }];
-  #   defaultGateway = {
-  #     address = "192.168.220.2";
-  #     interface = "eth0";
-  #   };
-  # };
+  # networking.networkmanager.enable = true;
+  networking = {
+    dhcpcd.enable = false;
+    domain = "alien.moon.mine";
+    search = [ "alien.moon.mine" ];
+    interfaces.eth0.ipv4.addresses = [{
+      address = "192.168.220.223";
+      prefixLength = 24;
+    }];
+    defaultGateway = {
+      address = "192.168.220.2";
+      interface = "eth0";
+    };
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
