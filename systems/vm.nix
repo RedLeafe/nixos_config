153530427eda520d8cbd9a95e8931fc56688273f
BirdeeHub <robindbor@gmail.com>
2024-10-02 15:21:41 -0700

    I am Spartacus
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   systems/vm.nix

--- systems/vm.nix
+++ systems/vm.nix
@@ -72,22 +72,18 @@ in {
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  networking.networkmanager.enable = true;
-  # networking = {
-  #   dhcpcd.enable = false;
-  #   interfaces.eth0.ipv4.addresses = [{
-  #     address = "192.168.220.223";
-  #     prefixLength = 24;
-  #   }];
-  #   interfaces.eth1.ipv4.addresses = [{
-  #     address = "10.100.136.223";
-  #     prefixLength = 24;
-  #   }];
-  #   defaultGateway = {
-  #     address = "192.168.220.2";
-  #     interface = "eth0";
-  #   };
-  # };
+  # networking.networkmanager.enable = true;
+  networking = {
+    dhcpcd.enable = false;
+    interfaces.eth0.ipv4.addresses = [{
+      address = "192.168.220.223";
+      prefixLength = 24;
+    }];
+    defaultGateway = {
+      address = "192.168.220.2";
+      interface = "eth0";
+    };
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
