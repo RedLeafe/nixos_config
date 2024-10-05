2b7defc2ffd3302124932aba32c759619c6ab4ee
BirdeeHub <robindbor@gmail.com>
2024-10-05 04:23:35 -0700

    verbose installer

--- systems/installer/default.nix
+++ systems/installer/default.nix
@@ -35,7 +35,7 @@ in {
       hostname=''${1:-'${hostname}'}
       username=''${2:-'${username}'}
       [ ! -d /home/nixos/nixos_config ] && cp -r /iso/nixos_config /home/nixos
-      sudo nixos-install --show-trace --flake /home/nixos/nixos_config#$hostname
+      sudo nixos-install --verbose --show-trace --flake /home/nixos/nixos_config#$hostname
       echo "please set password for user $username"
       sudo passwd --root /mnt $username
       umask 077
