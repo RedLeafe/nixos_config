95b3fd910296138dc72bc077a17edaf5f5342f88
BirdeeHub <robindbor@gmail.com>
2024-10-06 00:49:52 -0700

    We Had To Use Dark Magic To Make This Work
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/modules/WP/default.nix

--- common/modules/WP/default.nix
+++ common/modules/WP/default.nix
@@ -85,11 +85,6 @@ in
         extraConfig = /*php*/'' /* <?php */
           if ( !defined('ABSPATH') )
             define('ABSPATH', dirname(__FILE__) . '/');
-            /* echo ABSPATH; */
-          require_once ABSPATH . 'wp-admin/includes/plugin.php';
-          activate_plugin( 'ldap-login-for-intranet-sites/ldap-login-for-intranet-sites.php' );
-          /* activate_plugin( 'kubio/kubio.php' ); */
-          /* $_SERVER['HTTPS']='on'; */
         '';
       };
       services.httpd.enablePHP = true;
