7176757e3cc4d8205284ebd0a795320ad26cfca3
BirdeeHub <robindbor@gmail.com>
2024-10-06 07:13:52 -0700

    It'd be nice if type errors caused the compiler to issue a type error
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/modules/WP/default.nix

--- common/modules/WP/default.nix
+++ common/modules/WP/default.nix
@@ -122,7 +122,7 @@ in
         USER="${dbuser}"
         PASSWORD="''${1:-""}"
         # drop all databases except system ones
-        mysql -u$USER -p -e "SELECT CONCAT('DROP DATABASE `', SCHEMA_NAME, '`;') FROM information_schema.SCHEMATA WHERE SCHEMA_NAME NOT IN ('mysql', 'information_schema', 'performance_schema', 'sys');"
+        mysql -u$USER -p -e 'SELECT CONCAT("DROP DATABASE `", SCHEMA_NAME, "`;") FROM information_schema.SCHEMATA WHERE SCHEMA_NAME NOT IN ("mysql", "information_schema", "performance_schema", "sys");'
       '';
     in [
       restoreDBall
