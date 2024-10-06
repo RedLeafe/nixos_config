3bfba5ea6c13c7fd0b1e487727ee3d56de9c2cc5
BirdeeHub <robindbor@gmail.com>
2024-10-06 00:44:14 -0700

    I made leetle mistake
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/modules/WP/default.nix

--- common/modules/WP/default.nix
+++ common/modules/WP/default.nix
@@ -88,7 +88,7 @@ in
             /* echo ABSPATH; */
           require_once ABSPATH . 'wp-admin/includes/plugin.php';
           activate_plugin( 'ldap-login-for-intranet-sites/ldap-login-for-intranet-sites.php' );
-          activate_plugin( 'kubio/kubio.php' );
+          /* activate_plugin( 'kubio/kubio.php' ); */
           /* $_SERVER['HTTPS']='on'; */
         '';
       };
