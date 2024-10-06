87b6ac6b1f03f4d4bb77326192cbe7e1d8d93e02
BirdeeHub <robindbor@gmail.com>
2024-10-06 05:24:07 -0700

    WHO THE FUCK CAME UP WITH MAKE?
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   common/modules/WP/default.nix

--- common/modules/WP/default.nix
+++ common/modules/WP/default.nix
@@ -15,15 +15,15 @@
     in newpkgs.myWPext;
   in autobuilt // {
     # a place to put overrides:
-    ldap-login-for-intranet-sites = autobuilt.ldap-login-for-intranet-sites.overrideAttrs (prev: let
-      # TODO: figure out what to do with this.
-      cfg_ldap = ./miniorange-ldap-config.json;
-    in {
-      # TODO: do something with cfg_ldap
-      # instead of just overriding installPhase
-      # with the same exact string it already had
-      # installPhase = "mkdir -p $out; cp -R * $out/";
-    });
+    # ldap-login-for-intranet-sites = autobuilt.ldap-login-for-intranet-sites.overrideAttrs (prev: let
+    #   # TODO: figure out what to do with this.
+    #   cfg_ldap = ./miniorange-ldap-config.json;
+    # in {
+    #   # TODO: do something with cfg_ldap
+    #   # instead of just overriding installPhase
+    #   # with the same exact string it already had
+    #   # installPhase = "mkdir -p $out; cp -R * $out/";
+    # });
   };
 in
 {
